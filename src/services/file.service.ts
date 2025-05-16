9ab8446849a9d2bc83c76688c07b13e197e52f13
kranchit precha <bisza_i.d@hotmail.com>
2025-05-16 17:47:15 +0700

    update

--- src/services/file.service.ts
+++ src/services/file.service.ts
@@ -1,6 +1,20 @@
 import * as fs from "fs";
 
 export class FileService {
+  convertName(text: string) {
+    let itemString = String(text); // Convert to string
+    itemString = itemString.replace(/"/g, '""');
+
+    // If the string contains commas, double quotes, or newlines, enclose it in double quotes
+    if (
+      itemString.includes(",") ||
+      itemString.includes('"') ||
+      itemString.includes("\n")
+    ) {
+      itemString = `"${itemString}"`;
+    }
+    return itemString;
+  }
 
   // Function to convert data to CSV format
   convertToCSV(data: string[]): string {
@@ -8,24 +22,74 @@ export class FileService {
   }
 
   // Function to write CSV to a file
-  writeCSVToFile(csvContent: string, filePath: string) {
-    fs.readFile(filePath, 'utf8', (err, data) => {
-      if (data) {
-        if (err) throw err;
+  async writeCSVToFile(csvContent: string, filePath: string) {
+    try {
+      // Read the existing JSON file
+      const data = await fs.readFileSync(filePath, "utf8");
+      const updatedData = data + csvContent;
 
-        const newRow = csvContent;
-        const updatedData = data + newRow;
+      // Write the modified JSON data back to the file
+      await fs.writeFileSync(filePath, updatedData, "utf8");
 
-        fs.writeFile(filePath, updatedData, 'utf8', (err) => {
-          if (err) throw err;
-        });
-      } else {
-        fs.writeFile(filePath, csvContent, 'utf8', (err) => {
-          if (err) throw err;
-        });
-      }
+      console.log("Row appended successfully!");
+    } catch (err) {
+      console.error("Error appending row:", err);
 
-    });
+      const pathFail = `assets/reports/customer_fail.csv`;
+
+      // Read the existing JSON file
+      const dataOld = await fs.readFileSync(pathFail, "utf8");
+
+      // * Put Log On Not Found File
+      const data: any = [csvContent, err, new Date()];
+      const dataConvert = this.convertToCSV(data);
+      const updatedData = dataOld + dataConvert;
+
+      await fs.writeFileSync(pathFail, updatedData, "utf8");
+    }
   }
 
+  // Function to write CSV to a file
+  async writeJSONToFile(csvContent: any, filePath: string) {
+    try {
+      // Read the existing JSON file
+      const data = await fs.readFileSync(filePath, "utf8");
+      let jsonData = JSON.parse(data);
+
+      // Push the new row to the appropriate array
+      jsonData.data.push(csvContent); // Assuming 'data' is the array key
+
+      // Write the modified JSON data back to the file
+      await fs.writeFileSync(
+        filePath,
+        JSON.stringify(jsonData, null, 2),
+        "utf8"
+      );
+
+      console.log("Row appended successfully!");
+    } catch (err) {
+      console.error("Error appending row:", err);
+      // * Put Log On Not Found File
+      const fileJsonPath = `assets/reports/customer_fail.json`;
+
+      // Read the existing JSON file
+      const data = await fs.readFileSync(fileJsonPath, "utf8");
+      let jsonData = JSON.parse(data);
+
+      const dataJson = {
+        uuid: csvContent,
+        error: err,
+        date: new Date(),
+      };
+
+      // Push the new row to the appropriate array
+      jsonData.data.push(dataJson); // Assuming 'data' is the array key
+
+      await fs.writeFileSync(
+        fileJsonPath,
+        JSON.stringify(jsonData, null, 2),
+        "utf8"
+      );
+    }
+  }
 }
--- src/services/friend.service.ts
+++ src/services/friend.service.ts
@@ -1,5 +1,3 @@
-import * as fs from "fs";
-
 import { FileService } from "./file.service";
 import { LineService } from "./line.service";
 import { v4 as uuidv4 } from "uuid";
@@ -15,10 +13,15 @@ const fileService = new FileService();
 
 export class FriendService {
 
-  async addFriend() {
+   convertString(str) {
+    return str.replace(/^ /, '"');
+  }
 
+  async addFriend() {
+    const date = new Date();
     // let end = 0;
     let countFail = 0;
+    let startDate = date;
 
     // * Start Column Name
     const header_success = [
@@ -43,37 +46,38 @@ export class FriendService {
       "socialChannelId",
       "nickName",
       "status",
-    ]
+    ];
 
     // * Start Column Name
-    const header_fail = [
-      "uuid",
-      "error",
-      "date"
-    ]
-    const dateNow = new Date()
+    const header_fail = ["uuid", "error", "date"];
     // * Create File Header
     // Convert data to CSV and specify the file path
     const csvContent_success = fileService.convertToCSV(header_success);
-    const filePath_success = `assets/reports/customer_${dateNow.getTime()}_success.csv`;
+    const filePath_success = `assets/reports/customer_success.csv`;
 
     // Write the CSV to the specified file
-    fileService.writeCSVToFile(csvContent_success, filePath_success);
-
+    await fileService.writeCSVToFile(csvContent_success, filePath_success);
 
     // * Create File Header
     // Convert data to CSV and specify the file path
     const csvContent_fail = fileService.convertToCSV(header_fail);
-    const filePath_fail = `assets/reports/customer_${dateNow.getTime()}_fail.csv`;
+    const filePath_fail = `assets/reports/customer_fail.csv`;
 
     // Write the CSV to the specified file
-    fileService.writeCSVToFile(csvContent_fail, filePath_fail);
+    await fileService.writeCSVToFile(csvContent_fail, filePath_fail);
 
     // * ## Get Friend List Ids From Line
     /**
      * ['Uc296ae7....','Uc296ae7....','Uc296ae7....']
      */
     const Ids: any = await lineService.getFriendIds(token);
+    // const Ids = [
+    //   "Udca73857630cd84d5917c21ae163848d",
+    //   "Ucd90d00dcc8d4dd5510569da43414ee0",
+    //   "Udc05d580c2d28136b8289e8355c20d02",
+    //   "U4c33ac48e191f77d896393b5e9931a10",
+    //   "Uc70f8c385279cc68a2385e2916bed8a8",
+    // ];
     console.log("--- ---Ids Length---", Ids.length);
 
     for (const [index, id] of Ids.entries()) {
@@ -93,7 +97,7 @@ export class FriendService {
           id,
           profile && profile.pictureUrl ? profile.pictureUrl : "",
           "",
-          profile && profile.displayName ? '"' + profile.displayName + '"' : "",
+          profile && profile.displayName ? fileService.convertName(profile.displayName) : "",
           "",
           "",
           "",
@@ -114,25 +118,30 @@ export class FriendService {
 
         // Convert data to CSV and specify the file path
         const csvContent = fileService.convertToCSV(customer);
-        const filePath = `assets/reports/customer_${dateNow.getTime()}_success.csv`;
+        const filePath = `assets/reports/customer_success.csv`;
 
         // Write the CSV to the specified file
-        fileService.writeCSVToFile(csvContent, filePath);
+        await fileService.writeCSVToFile(csvContent, filePath);
       } catch (error) {
-
         countFail++;
-        const data = [id, error, new Date()]
+        const data = [id, error, new Date()];
+
+        const dataJson = {
+          uuid: id,
+          error: error,
+          date: new Date(),
+        };
 
         // Convert data to CSV and specify the file path
         const csvContent = fileService.convertToCSV(data);
-        const filePath = `assets/reports/customer_${dateNow.getTime()}_fail.csv`;
+        const filePath = `assets/reports/customer_fail.csv`;
 
         // Write the CSV to the specified file
-        fileService.writeCSVToFile(csvContent, filePath);
+        await fileService.writeCSVToFile(csvContent, filePath);
       }
       // end++;
     }
-
+    const endDate = new Date();
+    console.log(`startDate:[${startDate}] -> endDate:[${endDate}]`);
   }
-
 }
